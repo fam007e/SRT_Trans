ffb9085a35eefe6fef4ab58fb9d649746af579f4
Dark Knightz <faisalmoshiur@gmail.com>
2024-09-20 21:00:56 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 """Setup configuration for the SRT Translator package."""
 
-import os  # Standard library import
-from setuptools import setup  # Third-party import
+import os
+from setuptools import setup
 
 # Read the content of the README file
-with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+with open(
+    os.path.join(os.path.dirname(__file__), 'README.md'),
+    encoding='utf-8'
+) as f:
     long_description = f.read()
 
 setup(
     name='srt-translator',
     version='0.1',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Faisal Ahmed Moshiur',
+    author_email='faisalmoshiur+gitSRT@gmail.com',
     description='A script to translate .srt files from one language to another using GoogleTranslator.',
     long_description=long_description,
     long_description_content_type='text/markdown',
